f86c203b3648b1a1e611475aa33af98a0668a25c
Jagannath Arumugam <45625449+JagannathArumugam@users.noreply.github.com>
2023-03-08 22:16:58 +0530

    added modular code to calculate possible triangular arbitrage pairs and save it to a JSON file

--- Triangular-Arbitrage-Crypto-CeFi/main.py
+++ Triangular-Arbitrage-Crypto-CeFi/main.py
@@ -1,16 +1,41 @@
+import json
+
 import func_arbitrage
 
-"""
-    Step 0: Finding coins which can be traded
-    Exchange: Poloniex
-    API Docs: https://docs.legacy.poloniex.com/
-"""
 
-# Extract list of coins and prices from exchange
-coin_json = func_arbitrage.get_coin_tickers('https://poloniex.com/public?command=returnTicker')
+def step_0():
+    """
+        Step 0: Finding coins which can be traded
+        Exchange: Poloniex
+        API Docs: https://docs.legacy.poloniex.com/
+    """
 
-# Loop through each object and find the tradeable pairs
-coin_list = func_arbitrage.collect_tradeables(coin_json)
+    # Extract list of coins and prices from exchange
+    coin_json = func_arbitrage.get_coin_tickers('https://poloniex.com/public?command=returnTicker')
 
-print(len(coin_list))
-print(len(coin_json))
+    # Loop through each object and find the tradeable pairs
+    coin_list = func_arbitrage.collect_tradeables(coin_json)
+
+    # Return list of tradeable coins
+    return coin_list
+
+
+def step_1(coin_list):
+    """
+        Step : Structuring Triangular Pairs
+        Calculation Only
+    """
+
+    # Structure the list of tradeable triangular arbitrage pairs
+    structured_list = func_arbitrage.structure_triangular_pairs(coin_list)
+
+    # Save structured list
+    with open("structure_triangular_pairs.json", "w") as fp:
+        json.dump(structured_list, fp)
+
+
+""" MAIN """
+if __name__ == "__main__":
+    coin_list = step_0()
+    structured_pairs = step_1(coin_list)
+
